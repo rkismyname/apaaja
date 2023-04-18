debb8f7bbabc43ac4419641da7aca15e4228281a
dervishboy <ivan.andrian000@gmail.com>
2023-04-18 21:44:33 +0700

    Third Commit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,8 +4,9 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Windmill Dashboard</title>
-    <link href="{{ asset('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap') }}" rel="stylesheet" />
+    <title>Juragan Sertifikasi Indonesia</title>
+    <link href="{{ asset('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap') }}"
+        rel="stylesheet" />
     <link rel="stylesheet" href="{{ asset('/assets/css/style.css') }}" />
     <script src="{{ asset('https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js') }}" defer></script>
     <script src="{{ asset('./assets/js/init-alpine.js') }}"></script>
@@ -32,7 +33,7 @@
             @keydown.escape="closeSideMenu">
             <div class="py-4 text-gray-500 dark:text-gray-400">
                 <a class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="#">
-                    Windmill
+                    JuraganSertifikasi
                 </a>
                 <ul class="mt-6">
                     <li class="relative px-6 py-3">
@@ -187,8 +188,11 @@
             </div>
         </aside>
         <div class="flex flex-col flex-1 w-full">
+
+            {{-- header --}}
             @include('partials.header')
 
+            {{-- content --}}
             <main class="h-full overflow-y-auto">
                 @yield('content')
             </main>
--- resources/views/tes/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.main')
-
-@section('content')
-    <div class="bg-white">
-        INI ISINYA TAR CONTENT YA BONJING, BOTAK ANJING!
-    </div>
-@endsection
