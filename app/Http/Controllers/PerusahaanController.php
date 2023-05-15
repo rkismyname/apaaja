8f27cb9f6cc13ebb90e484153494a92e04a2ce70
dervishboy <ivan.andrian000@gmail.com>
2023-05-15 22:40:58 +0700

    Make Middleware User and Admin

--- app/Http/Controllers/PerusahaanController.php
+++ app/Http/Controllers/PerusahaanController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class PengajuanController extends Controller
+class PerusahaanController extends Controller
 {
     public function index() {
         return view('pengajuan.pengajuan');
