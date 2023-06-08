6f8b864cc57eecae8ae9a2c6a1eb54db6913be27
rkismyname <arkadiorjndra@gmail.com>
2023-06-09 00:42:35 +0700

    pengajuan_ttk and ttb button color

--- resources/views/customer/pengajuan/pengajuan_ttk.blade.php
+++ resources/views/customer/pengajuan/pengajuan_ttk.blade.php
@@ -87,11 +87,11 @@
                         <!-- Tombol submit dan lainnya -->
                         <div class="px-4 py-3 bg-white text-right sm:px-6 dark:bg-gray-800">
                             <button type="submit"
-                                class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500">
+                                class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-yellow-400 hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-400">
                                 Submit
                             </button>
                             <a href="{{ route('list.tk') }}"
-                                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                                class="inline-flex justify-center border text-sm border-transparent shadow-sm bg-yellow-400 hover:bg-yellow-600 text-white font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                                 Back
                             </a>
                         </div>
